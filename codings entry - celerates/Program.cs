949eccb1ace90a24d29bb6c198d5ae5e3b3c10e3
Aditya (竜) Syawal <adityasyawal2112@gmail.com>
2024-03-25 14:57:27 +0700

    init

---
+++ codings entry - celerates/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hi traveler, I've picked a number between 1-10. Can you guess the secret number?\n\n");
+        System.Console.WriteLine("Hi traveler, I've picked a number between 1-10. Can you guess the secret number?\n\n");
+    }
+}
